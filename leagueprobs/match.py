3aaf6c6febc8ea87a9b3fa30ab39fb7e0dcdfbc4
Felix Soubelet <felix.soubelet@protonmail.com>
2020-08-08 10:21:46 +0200

    better match

--- leagueprobs/match.py
+++ leagueprobs/match.py
@@ -13,6 +13,18 @@ class Match:
         self.week = week
         self.result = result
 
+    def __str__(self):
+        return f"Match({self.teams[1]}-{self.teams[1]})"
+
+    def __repr__(self):
+        if self.result:
+            return (
+                f"Match[{self.teams[1]}-{self.teams[1]} in week {self.week} with result "
+                f"{self.result}]"
+            )
+        else:
+            return f"Match[{self.teams[1]}-{self.teams[1]} in week {self.week}, winner tbd]"
+
     @property
     def winner(self) -> Union[str, None]:
         """
@@ -31,6 +43,25 @@ class Match:
             return self.teams[0]
         else:
             return self.teams[1]
+
+    @property
+    def loser(self) -> Union[str, None]:
+        """
+        Return the loser of this specific match, based on the result provided at instantiation.
+
+        Returns:
+            The name of the losing team, if there is a losing team (bo2 formats are weird).
+        """
+        if not self.result:
+            logger.debug(
+                f"Week {self.week}: '{self.teams[0]} vs {self.teams[1]}' either hasn't played or "
+                f"is a draw, indicating no loser yet"
+            )
+            return None
+        elif self.result[0] > self.result[1]:
+            return self.teams[1]
+        else:
+            return self.teams[0]
 
 
 def get_matches_from_json(json_file: Path) -> List[Match]:
